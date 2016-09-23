60cf9921776d201e05cafed20bf02dc2bfb41c4d
Sasikumar Karuppaiya <Sasikumar.Karuppaiya@balfour.com>
2016-09-23 16:57:05 +0530

    Laravel initial files

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+use Illuminate\Support\Facades\Log; 
+
+Route::get('/', function () {
+    // Log::debug($_SERVER);
+    // Log::info('Hello ', ['name' => 'Test']);
+    return view('welcome');
+});
